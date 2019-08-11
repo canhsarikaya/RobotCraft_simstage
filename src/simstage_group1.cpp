fa8294f451bda02819b28f4399c8922a8ba815e1
canhsarikaya <canhakansarikaya@gmail.com>
2019-08-11 18:02:35 +0100

    add comments

--- src/simstage_group1.cpp
+++ src/simstage_group1.cpp
@@ -1,3 +1,5 @@
+// Wall follower robot for continuous maps 
+
 #include <iostream>
 
 #include <cstdlib>
@@ -6,14 +8,14 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
 
-
+// Define states of the wall-follower inside the map
 #define BEGIN 1
 #define LEFT 2
 #define RIGHT 3
 #define STRAIGHT 4
-#define CHECK_DISTANCE 0.5
-#define LINEAR_SPEED 1
-#define ANGULAR_SPEED 0.5
+#define CHECK_DISTANCE 0.7
+#define LINEAR_SPEED 1.2
+#define ANGULAR_SPEED 0.7
 
 class ReactiveController
 {
@@ -22,23 +24,21 @@ private:
     ros::Publisher cmd_vel_pub;
     ros::Subscriber laser_sub;
 
+	//Create distance variables and set initial state to BEGIN
     double obstacle_distance_front = 999;
 	double obstacle_distance_right;
-	double obstacle_distance_left;
-
-
-
-	
-    bool robot_stopped;
+	double obstacle_distance_left;	
 	int state = BEGIN;
+
 
     geometry_msgs::Twist calculateCommand()
     {
         auto msg = geometry_msgs::Twist();
 
 
-      
-        switch(state){
+		// Use swith to alter cases according to momentary state of the robot 
+    	switch(state)
+		{
 			case BEGIN: 
 				return Begin();
 				break; 
@@ -51,13 +51,14 @@ private:
 			case STRAIGHT:
 				return GoStraight();
 				break;
-
-		}
+	}
 		
         
         return msg;
     }
 
+
+	// Define initial state, move forward until detecting an object then turn right
 	geometry_msgs::Twist Begin()
     {
 		auto msg = geometry_msgs::Twist();
@@ -71,14 +72,16 @@ private:
 	}
 
 
+	// Go straight if no frontal object exists and the right-side wall is no further the defined distance 
 	geometry_msgs::Twist GoStraight()
     {
 		auto msg = geometry_msgs::Twist();
 
-		if(obstacle_distance_front < CHECK_DISTANCE-0.2){
+		if(obstacle_distance_front < CHECK_DISTANCE - 0.2){  // Extract a value to avoid looping of state changes 
 			state = RIGHT;			
 			return msg;
 		}
+
 		if(obstacle_distance_left > CHECK_DISTANCE){
 			state = LEFT;
 			return msg;
@@ -89,7 +92,7 @@ private:
 	}
 
 
-
+	// Turn left if there exists no wall on the left, else change the state to STRAIGHT 
 	geometry_msgs::Twist GoLeft()
     {
 		auto msg = geometry_msgs::Twist();
@@ -98,13 +101,14 @@ private:
 		if(obstacle_distance_left < CHECK_DISTANCE){
 			state = STRAIGHT;
 			return msg;
-		}
+		}	
 
 		msg.angular.z = ANGULAR_SPEED;
 		return msg;
 	}
 
 
+	// Turn right if there exists a wall on the front, else change the state to STRAIGHT 
 	geometry_msgs::Twist GoRight()
     {
 		auto msg = geometry_msgs::Twist();
@@ -114,30 +118,32 @@ private:
 			return msg;
 		}
 		
-
 		msg.angular.z = -ANGULAR_SPEED;
-
-		}
+		return msg;
+	}
 
 
 
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     {
+		// Use sensor samples and range to measure distance between robot and the object 
+		// Use ROS_INFO to print the distance values and state of the robot
         obstacle_distance_right = *std::min_element(&msg->ranges[0], &msg->ranges[59]);
         ROS_INFO("Min distance to obstacle right: %f", obstacle_distance_right);
 
-		obstacle_distance_front = *std::min_element(&msg->ranges[70], &msg->ranges[169]);
+		obstacle_distance_front = *std::min_element(&msg->ranges[60], &msg->ranges[179]);
         ROS_INFO("Min distance to obstacle front: %f", obstacle_distance_front);
 
 		obstacle_distance_left = *std::min_element(&msg->ranges[180], &msg->ranges[239]);
         ROS_INFO("Min distance to obstacle left: %f", obstacle_distance_left);
 
-		ROS_INFO("state: %d", state);
+		ROS_INFO("State: %d", state);
     }
 
 
 public:
-    ReactiveController(){
+    ReactiveController()
+	{
         // Initialize ROS
         this->n = ros::NodeHandle();
 
@@ -149,7 +155,8 @@ public:
 
     }
 
-    void run(){
+    void run()
+	{
         // Send messages in a loop
         ros::Rate loop_rate(10);
 
@@ -172,8 +179,8 @@ public:
 };
 
 
-int main(int argc, char **argv){
-    // Initialize ROS
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "simstage_group1");
 
     // Create our controller object and run it
